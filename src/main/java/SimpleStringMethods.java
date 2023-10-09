f9b4f399de121c16d479e10172f98276989b3666
shamrin20 <shamrinmillet20@gmail.com>
2023-10-09 19:45:57 +0530

    complete

--- src/main/java/SimpleStringMethods.java
+++ src/main/java/SimpleStringMethods.java
@@ -14,7 +14,7 @@ public class SimpleStringMethods {
      * @return the length of string s.
      */
     public int getStringLength(String s){
-        return 0;
+        return s.length();
     }
 
     /**
@@ -24,7 +24,11 @@ public class SimpleStringMethods {
      * @return the character of String s that is located at position n.
      */
     public char getCharacterAtIndex(String s, int n){
-        return ' ';
+        if (n >=0 && n < s.length()) {
+            return s.charAt(n);
+        } else {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
     }
 
     /**
@@ -36,7 +40,7 @@ public class SimpleStringMethods {
      * @return the index of the first occurence of c within s.
      */
     public int getIndexGivenCharacter(String s, char c){
-        return 0;
+        return s.indexOf(c);
     }
 
 }
